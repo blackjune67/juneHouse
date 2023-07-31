6200fc95ccf8d643af3060639bd0bcf50ef48407
hajune <buea486@gmail.com>
2023-07-31 14:16:28 +0900

    allowed option edit

--- src/main/java/com/junehouse/config/WebConfig.java
+++ src/main/java/com/junehouse/config/WebConfig.java
@@ -6,11 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    // * CORS 문제해결
+    // * 모든 요청을 허용하도록 수정함
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 //        WebMvcConfigurer.super.addCorsMappings(registry);
         registry.addMapping("/**")
-                .allowedOrigins("*");
-//                .allowedOrigins("http://localhost:3000", "http://localhost:5173");
+//                .allowedOrigins("*");
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173");
     }
 }

589712a032aed0f15ce7799407fa190bdae8a0c7
hajune <buea486@gmail.com>
2023-07-31 16:17:49 +0900

    add read page

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import WriteView from '../views/WriteView.vue'
+import ReadView from "@/views/ReadView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,10 +15,12 @@ const router = createRouter({
       path: '/write',
       name: 'write',
       component: WriteView,
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       // component: () => import('../views/WriteView.vue')
+    },
+    {
+      path: '/read',
+      name: 'read',
+      component: ReadView,
     }
   ]
 })

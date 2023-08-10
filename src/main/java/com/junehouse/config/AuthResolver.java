02fadbaae42ad6c07892c77b1edbaed7c0127efe
hajune <buea486@gmail.com>
2023-08-10 21:06:22 +0900

    인증 관련 업데이트

--- src/main/java/com/junehouse/config/AuthResolver.java
+++ src/main/java/com/junehouse/config/AuthResolver.java
@@ -1,7 +1,10 @@
 package com.junehouse.config;
 
 import com.junehouse.config.dto.UserSession;
+import com.junehouse.domain.Session;
 import com.junehouse.exception.Unauthorized;
+import com.junehouse.repository.SessionRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -10,7 +13,11 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 @Slf4j
+@RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
+
+    private final SessionRepository sessionRepository;
+
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         log.info(">>> supportsParameter");
@@ -19,14 +26,16 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        log.info(">>> resolveArgument");
         String accessToken = webRequest.getHeader("Authorization");
+
+        // * 검증 자체를 안하고 통과 시킴
         if (accessToken == null || accessToken.equals("")) {
             throw new Unauthorized();
         }
 
-        // DB..
-        //        userSession.name = accessToken;
-        return new UserSession(1L);
+       Session byAccessToken = sessionRepository.findByAccessToken(accessToken)
+                .orElseThrow(Unauthorized::new);
+
+        return new UserSession(byAccessToken.getMember().getId());
     }
 }
--- src/main/java/com/junehouse/repository/SessionRepository.java
+++ src/main/java/com/junehouse/repository/SessionRepository.java
@@ -3,7 +3,10 @@ package com.junehouse.repository;
 import com.junehouse.domain.Session;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 // * 세션 Repository
 public interface SessionRepository extends CrudRepository<Session, Long> {
 
+    Optional<Session> findByAccessToken(String accessToken);
 }

121c1a9eed11c23b6ef8463848fe9014931d87be
hajune <buea486@gmail.com>
2023-08-11 17:55:08 +0900

    ResponseCookie 이용해 쿠키 로직으로 변경

--- src/main/java/com/junehouse/controller/AuthController.java
+++ src/main/java/com/junehouse/controller/AuthController.java
@@ -1,25 +1,41 @@
 package com.junehouse.controller;
 
-import com.junehouse.repository.MemberRepository;
 import com.junehouse.request.Login;
-import com.junehouse.response.SessionResponse;
 import com.junehouse.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
-    private final MemberRepository userRepository;
     private final AuthService authService;
 
     @PostMapping("/auth/login")
-    public SessionResponse login(@RequestBody Login login) {
-        log.info("==> {} ", login);
-        return new SessionResponse(authService.signin(login));
+    public ResponseEntity<?> login(@RequestBody Login login) {
+        String accessToken = authService.signin(login);
+
+        ResponseCookie responseCookie = ResponseCookie.from("SESSION", accessToken)
+                .domain("localhost") // todo 서버 환경에 따른 분리 필요
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Strict")
+                .build();
+
+        log.info("==> cookie={}", responseCookie.toString());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, responseCookie.toString())
+                .build();
     }
 }

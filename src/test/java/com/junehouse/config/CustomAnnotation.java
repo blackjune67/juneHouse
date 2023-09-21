e59173b4940be5c63c63feff4ea1838e6cf55b6c
hajune <buea486@gmail.com>
2023-09-21 17:30:51 +0900

    test 환경 lombok 설정

---
+++ src/test/java/com/junehouse/config/CustomAnnotation.java
@@ -0,0 +1,2 @@
+package com.junehouse.config;public interface CustomAnnotation {
+}
---
+++ src/test/java/com/junehouse/config/CustomMockSecurityContext.java
@@ -0,0 +1,6 @@
+package com.junehouse.config;
+
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+public class CustomMockSecurity implements WithSecurityContextFactory<> {
+}
